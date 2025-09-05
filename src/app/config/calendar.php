8c1be27499e26aff8ff157d32265f92e0d860f06
Karel Zavadil <zavadil@incomaker.com>
2025-09-05 11:36:07 +0200

    fixed login form

--- src/app/config/calendar.php
+++ src/app/config/calendar.php
@@ -5,5 +5,11 @@
 		// default e-mail address from which emails will be sent
 		'from_address' => 'rezervace@kosmetika-terka.cz',
 
-		'notify_email' => 'karelzav@gmail.com'
+		'notify_email' => 'karelzav@gmail.com',
+
+		'allow_weekends' => true,
+
+		'day_start_time' => '08:00',
+
+		'day_end_time' => '20:00'
 	];
